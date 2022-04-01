592bfde7813c55e7753516cfdb3fa2615d387507
daniel-ohh <danieloh@u.northwestern.edu>
2022-04-01 13:13:05 -0500

    HW2

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ME433.production.null
+CND_ARTIFACT_PATH_default=dist/default/production/ME433.production.null
