c24aaab3607dee7015e2c8507af93174c96758f3
daniel-ohh <danieloh@u.northwestern.edu>
2022-04-06 14:23:36 -0500

    HW3

--- HW3.X/main.c
+++ HW3.X/main.c
@@ -7,8 +7,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include<xc.h>           // processor SFR definitions
-#include<sys/attribs.h>  // __ISR macro
+#include <xc.h>           // processor SFR definitions
+#include <sys/attribs.h>  // __ISR macro
 
 // DEVCFG0
 #pragma config DEBUG = OFF // disable debugging
@@ -41,6 +41,9 @@
 #pragma config PMDL1WAY = OFF // allow multiple reconfigurations
 #pragma config IOL1WAY = OFF // allow multiple reconfigurations
 
+void ReadUART1(char * string, int maxLength);
+void  WriteUART1(const char * string);
+
 int main() {
 
     __builtin_disable_interrupts(); // disable interrupts while initializing things
@@ -61,16 +64,36 @@ int main() {
     DDPCONbits.JTAGEN = 0;
 
     // do your TRIS and LAT commands here
+    // UART1
+    U1RXRbits.U1RXR = 0b0000; // Set A2 to U1RX
+    RPB3Rbits.RPB3R = 0b0001; // Set B3 to U1TX
+    
+    U1MODEbits.BRGH = 0; // set baud to NU32_DESIRED_BAUD
+    U1BRG = ((48000000 / 230400) / 16) - 1;
+
+    // 8 bit, no parity bit, and 1 stop bit (8N1 setup)
+    U1MODEbits.PDSEL = 0;
+    U1MODEbits.STSEL = 0;
+
+    // configure TX & RX pins as output & input pins
+    U1STAbits.UTXEN = 1;
+    U1STAbits.URXEN = 1;
+    // configure hardware flow control without RTS and CTS
+   //U1MODEbits.UEN = 0;
+
+    // enable the uart
+    U1MODEbits.ON = 1;
 
     __builtin_enable_interrupts();
+    
+    char m[100];
     int i;
     while (1) {
-        // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
-        // remember the core timer runs at half the sysclk (48 MHz)
-
         if (PORTBbits.RB4 == 0) { //low when button is pushed 
             _CP0_SET_COUNT(0);
+            
             while (_CP0_GET_COUNT() < 48000) {;} //delay by 2 ms to debounce
+            
             if (PORTBbits.RB4 == 0) {  //if button is still pushed
                 _CP0_SET_COUNT(0);
                 for (i=0;i<4;i++) {  //two duty cycles
@@ -78,7 +101,44 @@ int main() {
                     LATAbits.LATA4=!PORTAbits.RA4;
                     _CP0_SET_COUNT(0);
                 }
-            }
-        }
+                sprintf(m,"Hello!\r\n");
+                WriteUART1(m);
+            }            
+         }
     }
 }
+
+void ReadUART1(char * message, int maxLength) {
+  char data = 0;
+  int complete = 0, num_bytes = 0;
+  // loop until you get a '\r' or '\n'
+  while (!complete) {
+    if (U1STAbits.URXDA) { // if data is available
+      data = U1RXREG;      // read the data
+      if ((data == '\n') || (data == '\r')) {
+        complete = 1;
+      } else {
+        message[num_bytes] = data;
+        ++num_bytes;
+        // roll over if the array is too small
+        if (num_bytes >= maxLength) {
+          num_bytes = 0;
+        }
+      }
+    }
+  }
+  // end the string
+  message[num_bytes] = '\0';
+}
+
+// Write a character array using UART3
+void WriteUART1(const char * string) {
+  while (*string != '\0') {
+    while (U1STAbits.UTXBF) {
+      ; // wait until tx buffer isn't full
+    }
+    U1TXREG = *string;
+    ++string;
+  }
+}
+
